c14031c44029db2600dc1f70c96c7bdaefc181ff
joao1barbosa <joao1.barbosa@outlook.com>
2024-09-10 15:16:03 -0300

    test: subservices tests

--- backend/src/clients/services/email-validation.service.spec.ts
+++ backend/src/clients/services/email-validation.service.spec.ts
@@ -2,20 +2,55 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { EmailValidationService } from './email-validation.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { ClientRepository } from '../repositories/clients.repository';
+import { BadRequestException } from '@nestjs/common';
 
 describe('EmailValidationService', () => {
   let service: EmailValidationService;
+  let repository: ClientRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [PrismaModule],
-      providers: [EmailValidationService, ClientRepository],
+      providers: [
+        EmailValidationService,
+        {
+          provide: ClientRepository,
+          useValue: {
+            findByEmail: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<EmailValidationService>(EmailValidationService);
+    repository = module.get<ClientRepository>(ClientRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('ensureEmailDoesNotExist', () => {
+    it('should not throw an exception if email does not exist', async () => {
+      // Arrange
+      const email = 'new@example.com';
+      (repository.findByEmail as jest.Mock).mockResolvedValue(null);
+
+      // Act & Assert
+      await expect(
+        service.ensureEmailDoesNotExist(email),
+      ).resolves.not.toThrow();
+    });
+
+    it('should throw BadRequestException if email already exists', async () => {
+      // Arrange
+      const email = 'existing@example.com';
+      (repository.findByEmail as jest.Mock).mockResolvedValue({ email });
+
+      // Act & Assert
+      await expect(service.ensureEmailDoesNotExist(email)).rejects.toThrow(
+        BadRequestException,
+      );
+    });
   });
 });
--- backend/src/operators/services/operator-validation.service.spec.ts
+++ backend/src/operators/services/operator-validation.service.spec.ts
@@ -2,20 +2,57 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { OperatorValidationService } from './operator-validation.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { OperatorsRepository } from 'src/operators/repositories/operators.repository';
+import { NotFoundException } from '@nestjs/common';
 
 describe('OperatorValidationService', () => {
   let service: OperatorValidationService;
+  let repository: OperatorsRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [PrismaModule],
-      providers: [OperatorValidationService, OperatorsRepository],
+      providers: [
+        OperatorValidationService,
+        {
+          provide: OperatorsRepository,
+          useValue: {
+            findOne: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<OperatorValidationService>(OperatorValidationService);
+    repository = module.get<OperatorsRepository>(OperatorsRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('validateOperatorExists', () => {
+    it('should not throw an exception if operator exists', async () => {
+      // Arrange
+      const operatorId = 1;
+      (repository.findOne as jest.Mock).mockResolvedValue({ id: operatorId });
+
+      // Act & Assert
+      await expect(
+        service.validateOperatorExists(operatorId),
+      ).resolves.not.toThrow();
+    });
+
+    it('should throw NotFoundException if operator does not exist', async () => {
+      // Arrange
+      const operatorId = 1;
+      (repository.findOne as jest.Mock).mockRejectedValue(
+        new Error('Not Found'),
+      );
+
+      // Act & Assert
+      await expect(service.validateOperatorExists(operatorId)).rejects.toThrow(
+        new NotFoundException(`O operador com ID ${operatorId} não existe.`),
+      );
+    });
   });
 });
